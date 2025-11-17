79d46d92445178c0675cd45c157fc4366e1a954c
unknown <azaria.rakha1@gmail.com>
2025-11-17 11:24:20 +0700

    tombol add

--- frontend/satria-uts/src/components/MainpageSection.jsx
+++ frontend/satria-uts/src/components/MainpageSection.jsx
@@ -10,13 +10,13 @@ const MainpageSection = () => {
         <nav className="navs">
           <ul className="uls">
             <li>
-              <a href="#">London</a>
+              <button type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">cek data siswa</button>
             </li>
             <li>
-              <a href="#">Paris</a>
+              <button type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Default</button>
             </li>
             <li>
-              <a href="#">Tokyo</a>
+              <button type="button" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Default</button>
             </li>
           </ul>
         </nav>
